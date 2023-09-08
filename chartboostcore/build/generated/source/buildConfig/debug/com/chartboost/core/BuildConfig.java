3a91b1caffba45d36401483c9fb4c74c1f527c3f
vu <vu.chau@chartboost.com>
2023-09-08 16:41:44 -0400

    Minor cleanup from own audit

--- chartboostcore/build/generated/source/buildConfig/debug/com/chartboost/core/BuildConfig.java
+++ chartboostcore/build/generated/source/buildConfig/debug/com/chartboost/core/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.chartboost.core";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String CHARTBOOST_CORE_VERSION = "0.1.0";
+  public static final String CHARTBOOST_CORE_VERSION = "0.2.0";
 }
